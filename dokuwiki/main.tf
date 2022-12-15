80cfe56dac743017a69f8368a12421a7c9518c46
JacobKaufman949 <jdkaufman00@gmail.com>
2022-12-15 09:42:15 +0000

    Adding changes to main.tf and playbook.yaml for the Dokuwiki project

---
+++ dokuwiki/main.tf
@@ -0,0 +1,71 @@
+variable "credentials_file" { 
+  default = "/home/jac5499/.config/gcloud/application_default_credentials.json"
+}
+
+variable "project" {
+  default = "cis91-project"
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-c"
+}
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials_file)
+  region  = var.region
+  zone    = var.zone 
+  project = var.project
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "dokuwiki-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "cis91"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_firewall" "default-firewall" {
+  name = "dokuwiki-firewall"
+  network = google_compute_network.vpc_network.name
+  allow {
+    protocol = "tcp"
+    ports = ["22", "80"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_disk" "default" {
+  name = "data"
+  
+}
+
+output "external-ip" {
+  value = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
+}
